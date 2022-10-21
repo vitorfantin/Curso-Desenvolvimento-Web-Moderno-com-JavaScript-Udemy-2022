e16a00382ae37ce9547eb934cc5e22733961ce82
Vitor Fantin <96424825+vitorfantin@users.noreply.github.com>
2022-10-20 22:40:15 -0300

    commit

---
+++ desafio_area.js
@@ -0,0 +1,3 @@
+let raio = 10;
+const areaCircunferencia = Math.PI * raio * raio;
+console.log("Resultado é: " + areaCircunferencia + " m²");
---
+++ desafio_troca.js
@@ -0,0 +1,11 @@
+// troca de dois números
+let a = 7;
+let b = 94;
+let c = 0;
+
+ c = a;
+ a = b;
+ b = c;
+
+console.log(a);
+console.log(b);
